f89ee442e81b7f983a08d8a2bc77a55c09bdb7f6
Dipanshu <dipanshukumar12345678@gmail.com>
2024-05-18 20:43:49 +0530

    push_react_code

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -5,12 +5,16 @@ import './App.css'
 
 function App() {
   // let counter = 0;
-  let [counter,setCounter] = useState(0);
+  const [counter,setCounter] = useState(0);
 
   const addValue = ()=>{
     // counter=counter+1;
-    if(counter+1<=20)
-    setCounter(counter+1);
+    if(counter+1<=20){
+      setCounter(counter+1);
+      // setCounter(prevcounter=>prevcounter+1);
+      // setCounter(prevcounter=>prevcounter+1);
+      // setCounter(prevcounter=>prevcounter+1);  Directly update +3
+    }
     // console.log(counter);
   }
 
